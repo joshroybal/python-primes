c4a39a98d09256296e24b03342be6370ff0e8ba5
Joshua E. Roybal <u4ysev@gmail.com>
2022-09-13 00:08:11 -0500

    Add files via upload

---
+++ test.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+
+import sys, time
+import prng, primes
+
+def message(t, t0):
+    string = 'Elapsed time: {:.3f} seconds'.format(t - t0)
+    print(string)
+
+def main(n):
+    print('n =', n)
+
+    print('Computing primes using primality predicate filter.')
+    t0 = time.time()
+    result = len(primes.primes(n))
+    message(time.time(), t0)
+    print(result)
+    print()
+
+    print('Computing primes using simple sieve.')
+    t0 = time.time()
+    result = len(primes.sieve(n))
+    message(time.time(), t0)
+    print(result)
+    print()
+
+    print('Computing primes using segmented sieve.')
+    t0 = time.time()
+    result = len(primes.segmented_sieve(n))
+    message(time.time(), t0)
+    print(result)
+    print()
+
+    print('Counting primes using primality predicate test.')
+    t0 = time.time()
+    result = primes.count_primes(n)
+    message(time.time(), t0)
+    string = '\u03A0(' + str(n) + ') = ' + str(result)
+    print(string)
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        sys.stderr.write('Usage: %s n\n' % sys.argv[0])
+        sys.exit(1)
+    try:
+        main(int(sys.argv[1]))
+    except Exception as error:
+        sys.stderr.write('%s\n' % error)
+        sys.exit(1)
